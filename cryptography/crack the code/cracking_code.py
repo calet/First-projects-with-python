f68c2510e42a66aebbb51329950c17e2d05f79c5
calet <lillbyggaren@live.se>
2022-08-16 19:53:39 +0200

    Update cracking_code.py
    fixed version, cracks random codes using both alphabet and numbers, the largest number i have tried with was 100 mil items and it restulted in 256.6 mil tries for correct sequence.

--- cryptography/crack the code/cracking_code.py
+++ cryptography/crack the code/cracking_code.py
@@ -1,68 +1,62 @@
 from random import choice
 
+class Amount:
+    def __init__(self):
+        self.counting = 0
+    
+    def c(self):
+        self.counting+=1
+
+def alphabet(chosen, i, tries):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    h = 0
+    
+    while h < len(alphabet):
+        letter_try = alphabet[h]
+        tries.c()
+        if tries.counting%1000 == 0:
+            print(tries.counting)
+        if letter_try != chosen[i]:
+            h+=1
+        else:
+            return letter_try
+        
+def numbers(chosen, i, tries):
+    number_try = 0
+   
+    c = int(chosen[i])
+    while number_try < 19:
+        if number_try == c:
+            return number_try
+        number_try+=1
+        tries.c()
+        if tries.counting%1000 == 0:
+            print(tries.counting)
+
 lottery = ['a', 'h', '6', '1', 'f', 'u', '3', 'o', '9', '10', '2', '18', '13', '5', '7']
 picked = 0
 chosen = []
-while picked < 4:
+while picked < 1000000:
     chosen.append(choice(lottery))
     picked+=1
 print("the winner is: " + ', '.join(chosen))
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-my_ticket = ['a', 'a', 'a', 'a']
-#my_ticket = []
 my_chosen_ticket = []
 all_tickets = 0
 list_place = 0
 alphabet_place = 0
 counter = 0
-list_times = 0
-times_to_remove = 1
-#while my_chosen_ticket != chosen:
-while counter < 20:
-    '''
-    while list_place < 4 and alphabet_place < len(alphabet):
-        my_ticket.insert(list_place, alphabet[alphabet_place])
-        list_place+=1
-    '''
-    #list_place = 0
-    if list_times == len(my_ticket):
-        list_place = 0
-        times_to_remove += 1
-    if list_times == len(my_ticket):
-        list_times = 0
-    while alphabet_place < len(alphabet):
-        
-        for i in range(times_to_remove):
-            del my_ticket[i]
-            #my_ticket.pop(list_place)
-        #for i in range(times_to_remove):
-        for i in range(times_to_remove):
-            my_ticket.insert(i, alphabet[alphabet_place])
-        alphabet_place += 1
-        print(my_ticket)
 
-    '''
-    point of program:
-    aaaa
-    1aaa
-    2aaa
-    3aaa
-    .
-    .
-    bbbb
-    1bbb
-    .
-    .
-    a1aa
-    b2bb
-    etc
-    '''
-    times_to_remove += 1
-    list_place += 1
-    list_times += 1
-    alphabet_place = 0
-    counter+=1
-    my_ticket = ['a', 'a', 'a', 'a']
-    print()
+list_times = 0
+
+my_tries = Amount()
+while counter < 4 and list_place < len(chosen):
+    save_letter = alphabet(chosen, list_place, my_tries)
+
+    if save_letter != None:
+        my_chosen_ticket.append(save_letter)
+    else:
+        save_number = numbers(chosen, list_place, my_tries)
+        my_chosen_ticket.append(save_number)
+    
+    list_place+=1
